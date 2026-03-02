56ae1e5b8ccaa78daebce2fb2f38a678eb5e77cc
Neha chauhan <anandvlog03@gmail.com>
2026-03-02 14:57:33 +0530

    add card more data

--- loginpage/src/pages/Home.tsx
+++ loginpage/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import demoData from "../json/data.json";
 
 const Home = () => {
   const username =
@@ -6,36 +7,40 @@ const Home = () => {
 
   return (
     <div>
-      <h1 className="text-red-500">
+      <h1 className="text-red-500 p-5">
         {username
           ? `Welcome to Dashboard, ${username}`
           : "Welcome to Dashboard"}
       </h1>
-      <div className="min-h-screen bg-gray-100 p-6">
+      <div className="min-h-screen bg-gray-100 pt-6">
         <div className="max-w-7xl mx-auto grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-          <div className="bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300 overflow-hidden">
-            <img
-              src="https://picsum.photos/600/400?random=1"
-              alt="Card Image"
-              className="w-full h-52 object-cover"
-            />
+          {demoData.map((item) => {
+            return (
+              <div
+                className="bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300 overflow-hidden"
+                key={item.id}
+              >
+                <img
+                  src={item.img}
+                  alt="Card Image"
+                  className="w-full h-52 object-cover"
+                />
 
-            <div className="p-5">
-              <h3 className="text-xl font-semibold text-gray-800 mb-2">
-                Mountain Adventure
-              </h3>
-              <p className="text-gray-600 text-sm mb-4">
-                Explore the beauty of mountains with fresh air and peaceful
-                nature views.
-              </p>
+                <div className="p-5">
+                  <h3 className="text-xl font-semibold text-gray-800 mb-2">
+                    {item.heading}
+                  </h3>
+                  <p className="text-gray-600 text-sm mb-4">{item.desc}</p>
 
-              <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">
-                Read More
-              </button>
-            </div>
-          </div>
+                  <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">
+                    Read More
+                  </button>
+                </div>
+              </div>
+            );
+          })}
 
-          <div className="bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300 overflow-hidden">
+          {/* <div className="bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300 overflow-hidden">
             <img
               src="https://picsum.photos/600/400?random=2"
               alt="Card Image"
@@ -54,7 +59,7 @@ const Home = () => {
                 Read More
               </button>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
